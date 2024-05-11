8d51903a4706583002857c4d01c174ff0a618cec
Jay Singh <learn.jayssingh@gmail.com>
2024-05-11 14:03:18 +0530

    part-03: exc 3.9-3.11 (phonebook)

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 
 const app = express()
 
@@ -27,6 +28,8 @@ let persons = [
 ]
 
 app.use(express.json())
+app.use(cors())
+app.use(express.static('dist'))
 
 morgan.token('post_data', function (req) { 
     if (req.method === 'POST') {
@@ -137,7 +140,7 @@ app.delete('/api/persons/:id', (request, response) => {
     response.status(204).end()
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+  console.log(`Server running on port ${PORT}`)
 })
